e6f8b92311d08f3f9e6f6709bbb836fd8f379597
João Vitor <149846523+joaobrxyz@users.noreply.github.com>
2024-10-10 21:34:56 -0300

    Initial commit

---
+++ ex001.sql
@@ -0,0 +1,9 @@
+create database cadastro;
+create table pessoas (
+	nome varchar(30),
+    nascimento tinyint,
+    sexo char(1),
+    peso float,
+    altura float,
+    nacionalidade varchar(20)
+);
