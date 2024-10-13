01300d6a689629f880e3f753a92dba296e4879cf
João Vitor <149846523+joaobrxyz@users.noreply.github.com>
2024-10-12 21:59:32 -0300

    Aula 07

---
+++ Aula 07 (PHPMyAdmin)/127_0_0_1.sql
@@ -0,0 +1,53 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 13/10/2024 às 02:56
+-- Versão do servidor: 10.4.32-MariaDB
+-- Versão do PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `exemplo`
+--
+CREATE DATABASE IF NOT EXISTS `exemplo` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `exemplo`;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `amigos`
+--
+
+CREATE TABLE IF NOT EXISTS `amigos` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(30) NOT NULL,
+  `sexo` enum('M','F') NOT NULL,
+  `telefone` varchar(15) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `nome` (`nome`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Despejando dados para a tabela `amigos`
+--
+
+INSERT INTO `amigos` (`id`, `nome`, `sexo`, `telefone`) VALUES
+(1, 'Maria', 'F', '2222-3333'),
+(2, 'João', 'M', '1111-2222'),
+(3, 'José', 'M', '3333-4444');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
