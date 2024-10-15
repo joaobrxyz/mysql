866551b3bcdc48eee6d46073c1cdd7c069dd6668
João Vitor <149846523+joaobrxyz@users.noreply.github.com>
2024-10-14 21:11:52 -0300

    Aula 08 pt3

---
+++ Aula 08 (Select)/ex011.sql
@@ -0,0 +1,19 @@
+select totaulas, count(*) from cursos
+group by totaulas
+order by totaulas;
+
+select carga, count(*) from cursos where totaulas = '30'
+group by carga;
+
+select ano, count(*) from cursos 
+where totaulas > 30
+group by ano
+having ano > 2013
+order by count(*) desc;
+
+select avg(carga) from cursos;
+
+select carga, count(*) from cursos
+where ano > 2015
+group by carga
+having carga > (select avg(carga) from cursos);
